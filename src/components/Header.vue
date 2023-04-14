c49b8de22ff94a71450dba04f8cf61bcb1727520
azoom-nguyen-ngoc-son <nguyen.ngoc.son@azoom.jp>
2023-04-14 10:14:47 +0700

    this is commit 1.1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1> this is commit 1</h1>
         <div class="headerPage" v-bind:style="{ 'background-color': backgroundHeader }">
             <box-icon class='icon' name='menu'></box-icon>
             <h1 class='title'>{{ titleHeader }} </h1>
